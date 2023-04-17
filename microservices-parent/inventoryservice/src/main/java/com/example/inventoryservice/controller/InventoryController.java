937055cca10f2cf0b6329bd98c171c0bc5cf1cda
DESKTOP-4T410RF\Dell <porchelvigunasekaran@gmail.com>
2023-04-17 09:45:13 +0200

    Inventory Controller which accepts skucode as request

---
+++ microservices-parent/inventoryservice/src/main/java/com/example/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,26 @@
+package com.example.inventoryservice.controller;
+
+import com.example.inventoryservice.dto.InventoryResponse;
+import com.example.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+@Slf4j
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryResponse> inStock(@RequestParam List<String> skuCode){
+        log.info("Inside Instock ==========="+skuCode);
+        return inventoryService.inStock(skuCode);
+    }
+}
